161d1cc61153d8629e9460a913c661abb339ff45
ghxst0 <lfoxy18@gmail.com>
2023-04-19 13:05:50 +0200

    fix bug with resource placement

--- src/main/java/com/codecool/marsexploration/logic/MapGenerator.java
+++ src/main/java/com/codecool/marsexploration/logic/MapGenerator.java
@@ -105,26 +105,26 @@ public class MapGenerator {
     }
 
     private void placeResources(TerrainElement resource, int numberOfResourcePoints){
-        Set<Coordinate> coordinatesNextToNeighbouringElement = new HashSet<>();
-        System.out.println("number of resource points: " + numberOfResourcePoints);
-        for(int i = 0; i < config.height(); i++){ // nem kell a height-böl kivonni 1-et
-            for(int j = 0; j < config.width(); j++){ // nem kell a width-böl kivonni 1-et
-                if ((i - 1 >= 0)) { // left - greater OR EQUAL
+        Set<Coordinate> coordinatesNextToNeighbouringElement = new HashSet<Coordinate>();
+        for(int i = 0; i < config.height(); i++){
+            for(int j = 0; j < config.width(); j++){
+                if(map[i][j] == ' '){
+                if ((i - 1 >= 0)) { // left
                     if (map[i - 1][j] == resource.getNeighbour()) {
                         coordinatesNextToNeighbouringElement.add(new Coordinate(i, j));
                     }
-                } else if(i + 1 < config.height()) { // right
+                }  if(i + 1 < config.width()) { // right
                     if (map[i + 1][j] == resource.getNeighbour()) {
                         coordinatesNextToNeighbouringElement.add(new Coordinate(i, j));
                     }
-                } else if(j + 1 < config.width()) { // up
+                }  if(j + 1 < config.height()) { // up
                     if(map[i][j + 1] == resource.getNeighbour()) {
                         coordinatesNextToNeighbouringElement.add(new Coordinate(i, j));
                     }
-                } else if((j - 1 >= 0 )) { // down  - greater OR EQUAL
+                }  if((j - 1 >= 0 )) { // down
                     if(map[i][j - 1] == resource.getNeighbour()){
                     coordinatesNextToNeighbouringElement.add(new Coordinate(i, j));
-                }}
+                }}}
             }
         }
         List<Coordinate> list=new ArrayList<>(coordinatesNextToNeighbouringElement);
@@ -133,9 +133,7 @@ public class MapGenerator {
                 Integer randomIndex = random.nextInt(list.size());
                 Coordinate randomCoordinate = list.get(randomIndex);
                 map[randomCoordinate.x()][randomCoordinate.y()] = resource.getSign();
-                System.out.println(randomCoordinate.x() + " " + randomCoordinate.y());
-                list.remove(randomCoordinate); // not list.remove(randomIndex)!
-                System.out.println(list.size());
+                list.remove(randomCoordinate);
             }
         } catch (Exception e){
             System.out.println("can't place all resources");
